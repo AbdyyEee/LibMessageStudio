73186d7407c24fda8fba8ef162b425dba46e2707
Abdyy <82438230+AbdyyEee@users.noreply.github.com>
2025-01-02 00:04:59 +0400

    Add flowchart functions

--- src/flowchart.c
+++ src/flowchart.c
@@ -1,1 +1,81 @@
 #include "flowchart.h"
+#include "libms.h"
+
+//matching
+LMS_Flowchart* LMS_InitFlowchart(const void* data) 
+{
+    LMS_Flowchart* flowchart = (LMS_Flowchart*)LMSi_Malloc(sizeof(LMS_Flowchart));
+    
+    flowchart->common.data = (const char*)data;
+
+    flowchart->flw3Offset = LMSi_SearchBlockByName(&flowchart->common, "FLW3");
+    flowchart->fen1Offset = LMSi_SearchBlockByName(&flowchart->common, "FEN1");
+    flowchart->ref1Offset = LMSi_SearchBlockByName(&flowchart->common, "REF1");
+    
+    return flowchart;
+}
+
+// matching
+void LMS_CloseFlowchart(LMS_Flowchart* flowchart)
+{
+    if (flowchart->common.blocks) {
+
+        LMSi_Free(flowchart->common.blocks);
+    }
+    
+    LMSi_Free(flowchart);
+}
+
+
+int LMS_GetFlowNodeIndex(const LMS_Flowchart* flowchart, const LMS_Node* node) {
+    // TODO 
+}
+
+int LMS_GetEntryNodeIndex(const LMS_Flowchart* flowchart, const char* label) {
+    // TODO
+}
+
+// Inferring return type
+const char* LMS_GetFlowParamText(LMS_Flowchart* flowchart, s32 offset) {
+    
+    LMS_BinaryBlock* flw3 = flowchart->common.blocks + flowchart->flw3Offset;
+
+    if ((offset < 0) || (flw3->size <= (u32)offset)) {
+        return NULL;
+    }
+    else {
+        return (char *)((int)flw3->data + offset);
+    }
+
+    return NULL;
+}
+
+const LMS_Node* LMS_GetNodeDataPtr(const LMS_Flowchart* flowchart, s32 index) {
+    if (flowchart->flw3Offset != -1) {
+        return (LMS_Node*)flowchart->common.blocks[flowchart->flw3Offset].data + (s32)index * 0x10 + 0x10;
+    }
+  return NULL;
+}
+
+s32 LMS_GetNodeNum(LMS_Flowchart* flowchart) 
+{
+    if (flowchart->flw3Offset != -1) {
+        return (u32)(flowchart->common).blocks[flowchart->flw3Offset].data;
+    }
+  return -1;
+}
+
+const u16* LMS_GetCaseIndexesFromBranchNode(LMS_Flowchart* flowchart, s32 index)
+{
+    LMS_BinaryBlock* flw3 = (flowchart->common).blocks + flowchart->flw3Offset;
+
+    if (flowchart->flw3Offset != -1) {
+        char* data = flw3->data;
+        char* nodeOffset = (char*)((s32)data + index * 0x10 + 0x10 );
+        
+        if (nodeOffset != NULL && (*nodeOffset == "\x02")) {
+            return (u16*)((s32)data + (u32)*(u16*)(nodeOffset + 0xe) * 2 + 0x10 + (u32)*flw3->data * 0x10);
+        }
+    }
+    return 0;
+}
