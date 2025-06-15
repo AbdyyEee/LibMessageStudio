46c5997af42846ad74f7c25999ace38c8d82cde0
Abdyy <abdyyy123456788@gmail.com>
2025-06-15 21:57:19 +0400

    Fix full strucuture of LMS_Node

--- include/flowchart.h
+++ include/flowchart.h
@@ -11,28 +11,28 @@ typedef struct LMS_Flowchart
 } LMS_Flowchart;
 
 typedef enum LMS_NodeTypes {
-    Message = 1,
-    Branch = 2,
-    Event = 3,
-    Entry = 4,
-    Jump = 5
+    MESSAGE = 1,
+    BRANCH = 2,
+    EVENT = 3,
+    ENTRY = 4,
+    JUMP = 5
 } LMS_NodeTypes;
 
 typedef struct LMS_Node 
 {
     LMS_NodeTypes type; // 0
-    u8 subType; // 1
+    s8 paramType; // 1
     u16 reserved; // 2
-    u16 subTypeValue; // 4
+    u32 paramValue; // 4
     LMS_NodeInfo* nodeInfo; // 8 
 } LMS_Node;
 
 typedef struct LMS_NodeInfo 
 {
-    u16 param1;
     u16 nextNodeID;
-    u16 param3;
-    u16 param4;
+    u16 identifier;
+    u16 short1;
+    u16 short2;
 
 } LMS_NodeInfo;
 
